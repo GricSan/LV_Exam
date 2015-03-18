da5157e6cc03357ba5559e77d852b47143090995
GricSan <gricsan@inbox.lv>
2015-03-18 20:17:32 +0200

    project modifications added

--- app/src/main/java/com/example/gricsan/lv_exam/ExamScreen.java
+++ app/src/main/java/com/example/gricsan/lv_exam/ExamScreen.java
@@ -3,7 +3,9 @@ package com.example.gricsan.lv_exam;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -15,6 +17,10 @@ public class ExamScreen extends ActionBarActivity {
     String[] mQuestions;
     TextView mQuestionText;
     TextView mQuestionAnswerText;
+    TextView mQuestionNumber;
+    private int mCurrQ;
+    private int mTotalQNum;
+    private View mNextQuestionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,29 +30,44 @@ public class ExamScreen extends ActionBarActivity {
         mQuestionPool = new ArrayList<>();
         mQuestionText = (TextView) findViewById(R.id.question_text);
         mQuestionAnswerText = (TextView) findViewById(R.id.answer_text);
+        mQuestionNumber = (TextView) findViewById(R.id.question_num);
         mQuestions = getResources().getStringArray(R.array.questions);
+        mNextQuestionButton = findViewById(R.id.show_nextq_button);
 
-        for(int i = 0; i <= mQuestions.length-1; i++){
+        for (int i = 0; i <= mQuestions.length - 1; i++) {
             mQuestionPool.add(new ExamQuestion(mQuestions[i]));
         }
         Collections.shuffle(mQuestionPool);
         mQuestionText.setText(mQuestionPool.get(0).getQuestionText());
+        mNextQuestionButton.setEnabled(false);
+        mTotalQNum = mQuestionPool.size();
+        mCurrQ = 1;
+        mQuestionNumber.setText(mCurrQ + "/" + mTotalQNum);
 
     }
 
+    @Override
+    public void onBackPressed() {
+        Toast.makeText(this,"Yeah, right... NOW FINISH IT!! BEYATCH!", Toast.LENGTH_SHORT).show();
+    }
+
     public void onClick(View view) {
-        switch(view.getId()){
+        switch (view.getId()) {
             case R.id.show_answer_button:
                 mQuestionAnswerText.setText(mQuestionPool.get(0).getRightAnswer());
+                mNextQuestionButton.setEnabled(true);
                 break;
             case R.id.show_nextq_button:
                 mQuestionPool.remove(0);
-                if(!mQuestionPool.isEmpty()){
+                mCurrQ++;
+                if (!mQuestionPool.isEmpty()) {
+                    mQuestionNumber.setText(mCurrQ + "/" + mTotalQNum);
                     mQuestionText.setText(mQuestionPool.get(0).getQuestionText());
-                }else{
+                } else {
                     finish();
                 }
-                    mQuestionAnswerText.setText("");
+                mQuestionAnswerText.setText("");
+                mNextQuestionButton.setEnabled(false);
                 break;
 //            case R.id.show_hint_button:
 //                mQuestionAnswerText.setText(mQuestionPool.get(0).getRightAnswer().charAt(0)+"....");
